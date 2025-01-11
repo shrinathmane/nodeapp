1ac15f2e6270a0114a21d331752b50d7a41ee29b
shrinathmane <shrinathmane43@gamil.com>
2025-01-11 20:20:13 +0530

    zala re

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('this is finally completed ');
+ res.send('koni mahanla hota ny zale shivaya rahat ny ');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
